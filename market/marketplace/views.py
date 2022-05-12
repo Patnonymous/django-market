26fad4c7a2442642b611ad1fea576bc21570f68b
Patrick_Williamson <patrickw9080@outlook.com>
2022-05-12 13:31:15 -0400

    Updated index view to use ListView and get featured only.

--- market/marketplace/views.py
+++ market/marketplace/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+
+from .models import MarketItem
 
 # Create your views here.
+
+
+class IndexView(generic.ListView):
+    template_name = 'marketplace/index.html'
+    context_object_name = 'featured_items_index'
+
+    def get_queryset(self):
+        return MarketItem.objects.filter(item_is_featured=True).order_by('-item_date_added')
 
 
 def index(request):
