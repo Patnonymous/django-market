9c2e59a140269d48f657f7a3a3d202257719b1b5
Patrick_Williamson <patrickw9080@outlook.com>
2022-05-12 12:13:01 -0400

    Registered Category and MarketItem models.

--- market/marketplace/admin.py
+++ market/marketplace/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+# Import models.
+from .models import Category, MarketItem
+
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(MarketItem)
