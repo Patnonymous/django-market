10099d8f41009a91a07916cbd3745cb5257266e1
Patrick_Williamson <patrickw9080@outlook.com>
2022-05-12 15:08:04 -0400

    Init tests.

--- market/marketplace/tests.py
+++ market/marketplace/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.utils import timezone
 
+from .models import MarketItem, Category
+
+
+# Add helper functions here.
+def quick_create_item(item_name, is_quantity_frozen, is_featured):
+    """
+    Quickly create an item by giving a name, setting if quantity froze, and setting
+    if the item is featured. All other properties will be set to some default values.
+    The date will be set to current date time.
+    """
+    time = timezone.now()
+    return MarketItem.objects.create(item_name=item_name, item_description='Quick created item.', item_price=12.34, item_quantity=50, item_freeze_quantity=is_quantity_frozen, item_category_id=1, item_is_featured=is_featured, item_date_added=time)
 # Create your tests here.
