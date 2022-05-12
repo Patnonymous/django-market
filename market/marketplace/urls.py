4082cea8ed4b513da617e55f3bb099a40ebcadf1
Patrick_Williamson <patrickw9080@outlook.com>
2022-05-12 14:10:50 -0400

    Added listings view. Updated templates to be reuable (for now).

--- market/marketplace/urls.py
+++ market/marketplace/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 # Set the url patterns.
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('listings/', views.ListingsView.as_view(), name='listings')
 ]
--- market/marketplace/views.py
+++ market/marketplace/views.py
@@ -9,10 +9,28 @@ from .models import MarketItem
 
 class IndexView(generic.ListView):
     template_name = 'marketplace/index.html'
-    context_object_name = 'featured_items_index'
+    context_object_name = 'items_index'
 
     def get_queryset(self):
         return MarketItem.objects.filter(item_is_featured=True).order_by('-item_date_added')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_header'] = 'Featured Market Items'
+        return context
+
+
+class ListingsView(generic.ListView):
+    template_name = 'marketplace/index.html'
+    context_object_name = 'items_index'
+
+    def get_queryset(self):
+        return MarketItem.objects.filter(item_is_featured=False).order_by('-item_date_added')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_header'] = 'User Market Listings'
+        return context
 
 
 def index(request):
