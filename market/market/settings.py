918ecef15c140500e4f72d101ae16a5006a493e1
Patrick_Williamson <patrickw9080@outlook.com>
2022-05-18 14:44:14 -0400

    Implemented djangos auth login and logout.

--- market/market/settings.py
+++ market/market/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Configure where to redirect a User after logging in.
+LOGIN_REDIRECT_URL = "/market"
+# Configure where to redirect a User after logging out.
+LOGOUT_REDIRECT_URL = "/market"
--- market/marketplace/urls.py
+++ market/marketplace/urls.py
@@ -1,7 +1,8 @@
 # Imports.
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
-# Import views.
+# Import my views.
 from . import views
 
 # Set the url patterns and the app namespace name.
@@ -9,5 +10,7 @@ app_name = 'market'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('listings/', views.ListingsView.as_view(), name='listings'),
-    path('item/<int:pk>/', views.ItemDetailView.as_view(), name='detail')
+    path('item/<int:pk>/', views.ItemDetailView.as_view(), name='detail'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
